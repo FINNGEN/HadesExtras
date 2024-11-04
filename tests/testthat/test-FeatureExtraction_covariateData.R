fc490b5323805b219bff5596f31f6eda1fe123fe
javier-gracia-tabuenca-tuni <javier.graciatabuenca@tuni.fi>
2024-11-04 08:55:36 +0200

    update test-FeatureExtraction_covariateData.Rkj

--- tests/testthat/test-FeatureExtraction_covariateData.R
+++ tests/testthat/test-FeatureExtraction_covariateData.R
@@ -20,55 +20,32 @@ test_that("covariateData_YearOfBirth works", {
   expect_true(all(yearOfBirthValues >= 1900 & yearOfBirthValues < 2000))
   covariate_control$covariates |> dplyr::collect() |> names() |>
     expect_equal(c("rowId", "covariateId", "covariateValue"))
+  covariate_control$covariates |> dplyr::collect() |> dplyr::distinct(covariateId) |>
+    dplyr::pull(covariateId) |> expect_equal(1041)
 })
 
 
 test_that("covariateData_YearOfBirth works aggregated", {
 
-  connection <- helper_createNewConnection()
-  #on.exit({DatabaseConnector::dropEmulatedTempTables(connection); DatabaseConnector::disconnect(connection)})
-
-  CohortGenerator::createCohortTables(
-    connection = connection,
-    cohortDatabaseSchema = testSelectedConfiguration$cohortTable$cohortDatabaseSchema,
-    cohortTableNames = CohortGenerator::getCohortTableNames(testSelectedConfiguration$cohortTable$cohortTableName)
-  )
-
-
-  cohortDefinitionSet <- CohortGenerator::getCohortDefinitionSet(
-    settingsFileName = here::here("inst/testdata/matching/Cohorts.csv"),
-    jsonFolder = here::here("inst/testdata/matching/cohorts"),
-    sqlFolder = here::here("inst/testdata/matching/sql/sql_server"),
-    cohortFileNameFormat = "%s",
-    cohortFileNameValue = c("cohortName"),
-    #packageName = "HadesExtras",
-    verbose = FALSE
-  )
-
-  generatedCohorts <- CohortGenerator::generateCohortSet(
-    connection = connection,
-    cdmDatabaseSchema = testSelectedConfiguration$cdm$cdmDatabaseSchema,
-    cohortDatabaseSchema = testSelectedConfiguration$cohortTable$cohortDatabaseSchema,
-    cohortTableNames = CohortGenerator::getCohortTableNames(testSelectedConfiguration$cohortTable$cohortTableName),
-    cohortDefinitionSet = cohortDefinitionSet,
-    incremental = FALSE
-  )
+  connection <- helper_createNewConnection(addCohorts = TRUE)
+  withr::defer({DatabaseConnector::dropEmulatedTempTables(connection); DatabaseConnector::disconnect(connection)})
 
   covariateSettings  <- covariateData_YearOfBirth()
 
   covariate_control <- FeatureExtraction::getDbCovariateData(
     connection = connection,
-    cohortTable = testSelectedConfiguration$cohortTable$cohortTableName,
-    cohortDatabaseSchema = testSelectedConfiguration$cohortTable$cohortDatabaseSchema,
-    cdmDatabaseSchema = testSelectedConfiguration$cdm$cdmDatabaseSchema,
+    cohortTable = "cohort",
+    cohortDatabaseSchema = test_cohortTableHandlerConfig$cohortTable$cohortDatabaseSchema,
+    cdmDatabaseSchema = test_cohortTableHandlerConfig$cdm$cdmDatabaseSchema,
     covariateSettings = covariateSettings,
-    cohortIds = 10,
-    aggregated = T
+    cohortIds = 1,
+    aggregated = TRUE
   )
 
   covariate_control$covariates  |> dplyr::collect()  |> nrow() |> expect_equal(0)
   covariate_control$covariatesContinuous  |> dplyr::collect()  |> nrow() |> expect_equal(1)
   covariate_control$covariatesContinuous  |> dplyr::collect()  |> names()  |>
     expect_equal(c("cohortDefinitionId", "covariateId", "countValue", "minValue", "maxValue", "averageValue", "standardDeviation", "medianValue", "p10Value", "p25Value", "p75Value", "p90Value"))
+  covariate_control$covariatesContinuous  |> dplyr::collect() |> dplyr::pull(covariateId)  |> expect_equal(1041)
 
 })
